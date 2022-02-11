680288618622ed75f2a237dcfa2044c360cfabb0
Hyeongrok-Oh <dhgudfhr1998@gmail.com>
2022-02-11 15:06:37 -0800

    edited signup

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,21 +1,24 @@
 import React, { useRef } from 'react'
-import { Form, Button, Card } from 'react-bootstrap'
+import { Form, Button } from 'react-bootstrap'
 
 export default function Signup() {
-  const nameRef = useRef()
+  const firstNameRef = useRef()
+  const lastNameRef = useRef()
   const genderRef = useRef()
   const emailRef = useRef()
   const passwordRef = useRef()
   const passwordConfirmRef = useRef()
   return (
     <>
-        <Card>
-            <Card.Body>
-                <h2 className="text-center mb-4">Account Information</h2>
+                <h1 className="text-center mb-4">Account Information</h1>
                 <Form>
                     <Form.Group id="Name">
                         <Form.Label>First Name</Form.Label>
-                        <Form.Control type="name" ref={nameRef} required />
+                        <Form.Control type="name" ref={firstNameRef} required />
+                    </Form.Group>
+                    <Form.Group id="Name">
+                        <Form.Label>Last Name</Form.Label>
+                        <Form.Control type="name" ref={lastNameRef} required />
                     </Form.Group>
                     <Form.Group id="gender">
                         <Form.Label>Gender</Form.Label>
@@ -37,12 +40,10 @@ export default function Signup() {
                     </Form.Group>
                     <Form.Group id="password-confirm">
                         <Form.Label>Password Confirmation</Form.Label>
-                        <Form.Control type="password" ref={passwordConfirmRef} required />
+                        <Form.Controlrv type="password" ref={passwordConfirmRef} required />
                     </Form.Group>
                     <Button className="w-100 mt-3" type="submit">Sign Up</Button>
                 </Form>
-            </Card.Body>
-        </Card>
         <div className="w-100 text-center mt-2">
             Already have an account? Log In
         </div>
