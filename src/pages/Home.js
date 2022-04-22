eb33b156b790742042febd405c3f99c5f4974118
Kellen Maier <kmaier7@uw.edu>
2022-04-22 14:05:02 -0700

    Adding filtering for users

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import HomepageFilter from '../components/HomepageFilter'
-import UserCard from '../components/UserCard'
 import { CardGroup, Row, Col} from 'react-bootstrap'
-import { useCollection } from '../hooks/useCollection'
+import { MapUsers } from '../components/HomepageFilter'
 
 const Home = () =>{
-  const { documents: users } = useCollection('users')
+
   return (
     <div>
       <Row>
@@ -15,7 +14,7 @@ const Home = () =>{
         <Col className='col-9'>
           <CardGroup>
             <Row  xs={1} md={2} lg={3}>
-              {users && <UserCard users={users}/>}
+              {MapUsers()}
             </Row>
           </CardGroup>
         </Col>
