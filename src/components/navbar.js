da09359025c030c5de214cde213d8d4979671d32
Hyeong Rok Oh <dhgudfhr1998@gmail.com>
2022-05-04 22:47:02 -0700

    added user profile button on nav bar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Button } from 'react-bootstrap'
-import {ReactComponent as AddFriend} from '../imgs/person-add.svg'
 import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from "../hooks/useAuthContext"
+import { db } from '../firebase/config'
+import { doc, getDoc} from 'firebase/firestore'
 import "../css/navbar.css";
 
 
 
 export default function Nav(){
   const { logout } = useLogout()
+  const { ...state} = useAuthContext()
+
+  const [imgUrl, setImgUrl] = useState()
+  const docRef = doc(db, "users", state.user.uid);
+  getDoc(docRef)
+  .then((docSnap) =>{
+    setImgUrl(docSnap.data().imgSrc)
+  }).catch(function(err) {
+    console.log(err)
+    setImgUrl('')
+  })
 
   return (
     <Navbar collapseOnSelect expand='md' className="navbar navbar-expand-sm bg-dark sticky-top d-flex justify-content-between">
@@ -46,8 +60,8 @@ export default function Nav(){
           <h5>Hello User</h5>
         </span>
           <li className="nav-item my-1">
-            <Button className='rounded-circle'>
-              <AddFriend />
+            <Button>
+              <img src={imgUrl} alt="user profile" style={{width: '5vh', objectFit: 'cover'}}></img>
             </Button>
           </li>
       </ul>
