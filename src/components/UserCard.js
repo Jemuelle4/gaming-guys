c55a971b22687ed202393b0c8deaf4dd33cc4867
Kellen Maier <kmaier7@uw.edu>
2022-05-08 21:08:43 -0700

    Update user cards to have correct color based on role

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -5,6 +5,7 @@ import UserCardModal from './UserCardModal'
 import { doc, updateDoc, arrayUnion } from 'firebase/firestore'
 import { db } from '../firebase/config'
 import { useAuthContext } from "../hooks/useAuthContext"
+import classNames from "classnames"
 import poster from '../poster.png'
 import "../css/components.css";
 
@@ -26,50 +27,53 @@ const UserCard = ({ user, userKey }) =>{
 
   const [modalShow, setModalShow] = useState(false);
   if(user){
-  return (
-      <Col className='m-5' style={{ width: '20rem', height: '20rem'}}>
-        <Card style={{ cursor:'pointer', width: '20rem', height: '20rem', border:'none'}} onClick={() => setModalShow(true)}>
-          <Card.Img style={{ width: '20rem', height: '20rem'}} src={user.imgSrc? user.imgSrc : poster} alt="Card image" />
-          <Card.ImgOverlay>
-            <Button style={{ width: '3rem', height: '3rem'}} className='rounded-circle' onClick={handleClick}>
-              <AddFriend/>
-            </Button>
-            <div className="information">
-              <Row>
-                <Col>
-                  <Card.Title>{user.displayName}</Card.Title>
-                </Col>
-                <Col>
-                <Card.Text>{user.role}</Card.Text>
-                </Col>
-              </Row>
-              <Row>
-                <Col sm={6} className='d-flex justify-content-center'>
-                  {getEmblem(user.rank)}
-                </Col>
-                <Col sm={3} className='d-flex justify-content-center'>
-                  {getRoleIcon(user.rank, user.fav_role[0])}
-                </Col>
-                <Col sm={3} className='d-flex justify-content-center'>
-                  {getRoleIcon(user.rank, user.fav_role[1])}
-                </Col>
-              </Row>
-              <Row>
-                <Col sm={6} className='d-flex justify-content-center'>
-                  <Card.Text>{user.rank}</Card.Text>
-                </Col>
-                <Col sm={6} className='d-flex justify-content-center'>
-                  <Card.Text>{user.fav_role[0]}/{user.fav_role[1]}</Card.Text>
-                </Col>
-              </Row>
-            </div>
-            
-          </Card.ImgOverlay>
-        </Card>
-        <UserCardModal user={user} show={modalShow} onHide={() => setModalShow(false)} />
-      </Col>
-      
-  );
+    let coach = (user.role === 'Coach')
+    let learner = (user.role === 'Learner')
+    let teammate = (user.role === 'Teammate')
+    return (
+        <Col className='m-5' style={{ width: '20rem', height: '20rem'}}>
+          <Card style={{ cursor:'pointer', width: '20rem', height: '20rem', border:'none'}} onClick={() => setModalShow(true)}>
+            <Card.Img style={{ width: '20rem', height: '20rem'}} src={user.imgSrc? user.imgSrc : poster} alt="Card image" />
+            <Card.ImgOverlay>
+              <Button style={{ width: '3rem', height: '3rem'}} className='rounded-circle' onClick={handleClick}>
+                <AddFriend/>
+              </Button>
+              <div className={classNames('information', {coach: coach, learner: learner, teammate: teammate})}>
+                <Row>
+                  <Col>
+                    <Card.Title>{user.displayName}</Card.Title>
+                  </Col>
+                  <Col>
+                  <Card.Text>{user.role}</Card.Text>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col sm={6} className='d-flex justify-content-center'>
+                    {getEmblem(user.rank)}
+                  </Col>
+                  <Col sm={3} className='d-flex justify-content-center'>
+                    {getRoleIcon(user.rank, user.fav_role[0])}
+                  </Col>
+                  <Col sm={3} className='d-flex justify-content-center'>
+                    {getRoleIcon(user.rank, user.fav_role[1])}
+                  </Col>
+                </Row>
+                <Row>
+                  <Col sm={6} className='d-flex justify-content-center'>
+                    <Card.Text>{user.rank}</Card.Text>
+                  </Col>
+                  <Col sm={6} className='d-flex justify-content-center'>
+                    <Card.Text>{user.fav_role[0]}/{user.fav_role[1]}</Card.Text>
+                  </Col>
+                </Row>
+              </div>
+              
+            </Card.ImgOverlay>
+          </Card>
+          <UserCardModal user={user} show={modalShow} onHide={() => setModalShow(false)} />
+        </Col>
+        
+    );
   }
 }
 
