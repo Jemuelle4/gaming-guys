9e4ed5fbfe308052c5ff0dd1c2796f97365563fd
Kellen Maier <kmaier7@uw.edu>
2022-05-12 17:54:06 -0700

    Add functionality to connections page

--- src/pages/Connections.js
+++ src/pages/Connections.js
@@ -1,13 +1,75 @@
-import React from 'react'
-import { Card, CardGroup, Container, Row, Col, Button } from 'react-bootstrap'
+import React, {useState} from 'react'
+import { Card, CardGroup, Row, Col, Button } from 'react-bootstrap'
 import Navbar from '../components/navbar'
 import { useCollection } from '../hooks/useCollection'
 import SelectedConnection from '../components/SelectedConnection'
+import { doc, updateDoc, arrayUnion, arrayRemove } from 'firebase/firestore'
+import { db } from '../firebase/config'
+import { useAuthContext } from "../hooks/useAuthContext"
+import AcceptedConnection from '../components/AcceptedConnection'
 
 const Connections = ({uid}) =>{
   const { documents: users } = useCollection('users')
+  const { ...state} = useAuthContext()
+  let [selectedUser, setSelectedUser] = useState()
+  
+
+  function handleAccept(e, uid) {
+    e.preventDefault()
+    const ref1 = doc(db, "users", state.user.uid)
+		updateDoc(ref1, {
+      receivedBy: arrayRemove(uid),
+      connections: arrayUnion(uid)
+    })
+    const ref2 = doc(db, "users", uid) 
+    updateDoc(ref2, {
+      requestedTo: arrayRemove(state.user.uid),
+      connections: arrayUnion(state.user.uid)
+    })
+  }
+
+  function handleDecline(e, uid) {
+    e.preventDefault()
+    const ref1 = doc(db, "users", state.user.uid)
+		updateDoc(ref1, {
+      receivedBy: arrayRemove(uid)
+    })
+    const ref2 = doc(db, "users", uid) 
+    updateDoc(ref2, {
+      requestedTo: arrayRemove(state.user.uid)
+    })
+  }
+
+  function mapConnectionRequests(receivedBy) {
+    if(receivedBy) {
+      return (receivedBy.map((receivedID) => {
+        let receivedUser = users.find(user => user.id === receivedID)
+        return (
+          <Row className='p-2' key={uid}>
+            {getConnection(receivedUser)}
+          </Row>
+        )
+      }))
+    }
+  }
+  
+  function getConnection(user) {
+    if(user) {
+      return(
+          <Card className='bg-dark'>
+            <Row>
+              <Col><Card.Title>{user.displayName}</Card.Title></Col>
+              <Col><Card.Body>{user.rank}/{user.role}</Card.Body></Col>
+              <Col><Button variant='success' onClick={e => handleAccept(e, user.id)}>Accept</Button></Col>
+              <Col><Button variant='danger' onClick={e => handleDecline(e, user.id)}>Decline</Button></Col>
+            </Row>
+          </Card>
+      )
+    }
+  }
+
   if(users) {
-    let currentUser = users.find(user => user.id === uid)
+    const currentUser = users.find(user => user.id === uid)
     return (
       <div style={{marginTop: '100px'}}>
         <Navbar/>
@@ -15,38 +77,21 @@ const Connections = ({uid}) =>{
           <Col className='col-3'>
             <h1>Connection Requests</h1>
             <CardGroup>
-              {currentUser.receivedBy.map((uid) => {
-                let user = users.find(user => user.id === uid)
-                return (
-                  <Row className='p-2' key={uid}>
-                    {getConnection(user)}
-                  </Row>
-                )
-              })}
+              {mapConnectionRequests(currentUser.receivedBy)}
             </CardGroup>
           </Col>
           <Col className='col-6'>
-            <h1>This is the connections the user has</h1>
+            <h1>Connections</h1>
+            {currentUser.connections.map(connectionID => (
+              <AcceptedConnection uid={connectionID} setSelectedUser={setSelectedUser}/>
+            ))}
           </Col>
           <Col className='col-3'>
-            <SelectedConnection user={currentUser} />
+            <SelectedConnection user={selectedUser} currentUserID={currentUser.id}/>
           </Col>
         </Row>
       </div>
     );
   }
-}
-
-function getConnection(user) {
-  return(
-      <Card className='bg-dark'>
-        <Row>
-          <Col><Card.Title>{user.displayName}</Card.Title></Col>
-          <Col><Card.Body>{user.rank}/{user.role}</Card.Body></Col>
-          <Col><Button variant='success'>Accept</Button></Col>
-          <Col><Button variant='danger'>Decline</Button></Col>
-        </Row>
-      </Card>
-  )
 }
 export default Connections;
