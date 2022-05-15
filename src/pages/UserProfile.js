58e6a321b043b666d2bffac42495844a49295537
Hyeong Rok Oh <dhgudfhr1998@gmail.com>
2022-05-14 23:23:50 -0700

    add css to profile edititng page

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -26,6 +26,8 @@ const About = ({uid}) =>{
           return <CommuncationComponent/>
       } else if(mode === 'role'){
           return <RoleComponent/>
+      } else {
+        return <InGameComponent/>
       }
   }
 
@@ -41,59 +43,63 @@ const About = ({uid}) =>{
     return (
       <div style={{marginTop: '100px'}}>
         <Navbar />
-        <button value="ingame" onClick={handleClick}>In Game Information</button>
-        <button value="communication" onClick={handleClick}>Communication</button>
-        <button value="role" onClick={handleClick}>Role / Profile Image</button>
-        {component()}
-        <div>
-          <Card style={{ width: '20rem', height: '20rem', border:'none'}}>
-            <Card.Img style={{ width: '20rem', height: '20rem'}} src={user.imgSrc? user.imgSrc : poster} alt="Card image" />
-            <Card.ImgOverlay>						
-              <div className={classNames('information', {coach: coach, learner: learner, teammate: teammate})}>
-              <Row className='mb-2 g-0'>
-                  <Col xs={6}>
-                    <Card.Title className='ms-3'>{user.displayName}</Card.Title>
-                  </Col>
-                  <Col xs={6} className='d-flex justify-content-end'>
-                  <Card.Text className='me-3'>{user.role}</Card.Text>
-                  </Col>
-                </Row>
-                <Row className='g-0'>
-                  <Col xs={4} className='d-flex justify-content-center'>
-                    {getEmblem(user.rank)}
-                  </Col>
-                  <Col xs={4} className='d-flex justify-content-center'>
-                    {getRoleIcon(user.rank, user.fav_role[0])}
-                  </Col>
-                  <Col xs={4} className='d-flex justify-content-center'>
-                    {getRoleIcon(user.rank, user.fav_role[1])}
-                  </Col>
-                </Row>
-                <Row className="pb-2 g-0">
-                  <Col xs={4} className='d-flex justify-content-center'>
-                    <Card.Text>{user.rank}</Card.Text>
-                  </Col>
-                  <Col xs={4} className='d-flex justify-content-center'>
-                    <Card.Text>{user.fav_role[0]}</Card.Text>
-                  </Col>
-                  <Col xs={4} className='d-flex justify-content-center'>
-                    <Card.Text>{user.fav_role[1]}</Card.Text>
-                  </Col>
-                </Row>
-              </div>
-            </Card.ImgOverlay>
-          </Card>
-          <div className='extracontent'>
-            <h3>Strengths</h3>
-            <ul className='strength'>
-              <li>Strength 1</li>
-              <li>Strength 2</li>
-            </ul>
-            <h3>Weaknesses</h3>
-            <ul className='weakness'>
-              <li>Weakness 1</li>
-            </ul>
-            <h3>Communication Platforms</h3>
+        <div style={{display: 'flex'}}>
+          <div style={{display: 'block'}}>
+            <button value="ingame" onClick={handleClick}>In Game Information</button>
+            <button value="communication" onClick={handleClick}>Communication</button>
+            <button value="role" onClick={handleClick}>Role / Profile Image</button>
+          </div>
+          {component()}
+          <div>
+            <Card style={{ width: '20rem', height: '20rem', border:'none'}}>
+              <Card.Img style={{ width: '20rem', height: '20rem'}} src={user.imgSrc? user.imgSrc : poster} alt="Card image" />
+              <Card.ImgOverlay>						
+                <div className={classNames('information', {coach: coach, learner: learner, teammate: teammate})}>
+                <Row className='mb-2 g-0'>
+                    <Col xs={6}>
+                      <Card.Title className='ms-3'>{user.displayName}</Card.Title>
+                    </Col>
+                    <Col xs={6} className='d-flex justify-content-end'>
+                    <Card.Text className='me-3'>{user.role}</Card.Text>
+                    </Col>
+                  </Row>
+                  <Row className='g-0'>
+                    <Col xs={4} className='d-flex justify-content-center'>
+                      {getEmblem(user.rank)}
+                    </Col>
+                    <Col xs={4} className='d-flex justify-content-center'>
+                      {getRoleIcon(user.rank, user.fav_role[0])}
+                    </Col>
+                    <Col xs={4} className='d-flex justify-content-center'>
+                      {getRoleIcon(user.rank, user.fav_role[1])}
+                    </Col>
+                  </Row>
+                  <Row className="pb-2 g-0">
+                    <Col xs={4} className='d-flex justify-content-center'>
+                      <Card.Text>{user.rank}</Card.Text>
+                    </Col>
+                    <Col xs={4} className='d-flex justify-content-center'>
+                      <Card.Text>{user.fav_role[0]}</Card.Text>
+                    </Col>
+                    <Col xs={4} className='d-flex justify-content-center'>
+                      <Card.Text>{user.fav_role[1]}</Card.Text>
+                    </Col>
+                  </Row>
+                </div>
+              </Card.ImgOverlay>
+            </Card>
+            <div className='extracontent'>
+              <h3>Strengths</h3>
+              <ul className='strength'>
+                <li>Strength 1</li>
+                <li>Strength 2</li>
+              </ul>
+              <h3>Weaknesses</h3>
+              <ul className='weakness'>
+                <li>Weakness 1</li>
+              </ul>
+              <h3>Communication Platforms</h3>
+            </div>
           </div>
         </div>
       </div>
